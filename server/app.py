0c480605a9b5b3c0ef08b6e973f39894115f4acb
Syed Naveed Abbas <naveedabs31@gmail.com>
2025-06-01 13:14:08 +0500

    '.'

--- server/app.py
+++ server/app.py
@@ -6,7 +6,6 @@ from PIL import Image
 import io
 import os
 from flask_cors import CORS
-from waitress import serve
 
 app = Flask(__name__)
 CORS(app)
@@ -77,4 +76,5 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
+    from waitress import serve
     serve(app, host="0.0.0.0", port=5000)
